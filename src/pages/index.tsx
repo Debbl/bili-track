fa4aee9225b89b55640372f3d45e5a9eb82ce463
Debbl <me@aiwan.run>
2023-04-11 22:41:44 +0800

    feat: add loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useRequest } from "ahooks";
+import { useEffect, useMemo, useState } from "react";
 import ReactECharts from "~/components/ReactECharts";
 
 type Data = {
@@ -52,71 +53,142 @@ interface Option {
   ];
 }
 
+async function getData() {
+  return await (await fetch("/api/get-data")).json();
+}
+
 export default function Home() {
-  const [options, setOptions] = useState<Option[]>([]);
+  const { data, loading } = useRequest<Data, any>(() => getData());
 
-  useEffect(() => {
-    async function effect() {
-      const data: Data = await (await fetch("/api/get-data")).json();
-      const rawOptions: RawOptions = {};
-      const options: Option[] = [];
-
-      data.forEach((d) => {
-        if (!rawOptions[d.fields.mid]) {
-          rawOptions[d.fields.mid] = {
-            name: d.fields.name,
-            fields: [],
-          };
-        }
-        rawOptions[d.fields.mid].fields.push(d.fields);
-      });
-
-      for (const key in rawOptions) {
-        const o = rawOptions[key];
-        const source = o.fields.map((f) => {
-          const d = new Date(f.update_time);
-          return [
-            `${d.getFullYear()}/${
-              d.getMonth() + 1
-            }/${d.getDate()} ${d.getHours()}:${d
-              .getMonth()
-              .toString()
-              .padStart(2, "0")}`,
-            f.follower,
-          ];
-        });
-        options.push({
-          title: {
-            text: o.name,
-          },
-          dataset: {
-            source,
-          },
-          xAxis: {
-            type: "category",
-          },
-          yAxis: {
-            type: "value",
-          },
-          series: [
-            {
-              type: "line",
-              label: {
-                show: true,
-                position: "bottom",
-              },
-            },
-          ],
-        });
+  const options = useMemo<Option[]>(() => {
+    const rawOptions: RawOptions = {};
+    const options: Option[] = [];
+    if (!data) return [];
+    data.forEach((d) => {
+      if (!rawOptions[d.fields.mid]) {
+        rawOptions[d.fields.mid] = {
+          name: d.fields.name,
+          fields: [],
+        };
       }
-
-      setOptions(options);
+      rawOptions[d.fields.mid].fields.push(d.fields);
+    });
+    for (const key in rawOptions) {
+      const o = rawOptions[key];
+      const source = o.fields.map((f) => {
+        const d = new Date(f.update_time);
+        return [
+          `${d.getFullYear()}/${
+            d.getMonth() + 1
+          }/${d.getDate()} ${d.getHours()}:${d
+            .getMonth()
+            .toString()
+            .padStart(2, "0")}`,
+          f.follower,
+        ];
+      });
+      options.push({
+        title: {
+          text: o.name,
+        },
+        dataset: {
+          source,
+        },
+        xAxis: {
+          type: "category",
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [
+          {
+            type: "line",
+            label: {
+              show: true,
+              position: "bottom",
+            },
+          },
+        ],
+      });
     }
-    effect();
-  }, []);
+    return options;
+  }, [data]);
+
+  // useEffect(() => {
+  //   async function effect() {
+  //     const data: Data = await (await fetch("/api/get-data")).json();
+  //     const rawOptions: RawOptions = {};
+  //     const options: Option[] = [];
+
+  //     data.forEach((d) => {
+  //       if (!rawOptions[d.fields.mid]) {
+  //         rawOptions[d.fields.mid] = {
+  //           name: d.fields.name,
+  //           fields: [],
+  //         };
+  //       }
+  //       rawOptions[d.fields.mid].fields.push(d.fields);
+  //     });
+
+  //     for (const key in rawOptions) {
+  //       const o = rawOptions[key];
+  //       const source = o.fields.map((f) => {
+  //         const d = new Date(f.update_time);
+  //         return [
+  //           `${d.getFullYear()}/${
+  //             d.getMonth() + 1
+  //           }/${d.getDate()} ${d.getHours()}:${d
+  //             .getMonth()
+  //             .toString()
+  //             .padStart(2, "0")}`,
+  //           f.follower,
+  //         ];
+  //       });
+  //       options.push({
+  //         title: {
+  //           text: o.name,
+  //         },
+  //         dataset: {
+  //           source,
+  //         },
+  //         xAxis: {
+  //           type: "category",
+  //         },
+  //         yAxis: {
+  //           type: "value",
+  //         },
+  //         series: [
+  //           {
+  //             type: "line",
+  //             label: {
+  //               show: true,
+  //               position: "bottom",
+  //             },
+  //           },
+  //         ],
+  //       });
+  //     }
+
+  //     setOptions(options);
+  //   }
+  //   effect();
+  // }, []);
 
   return (
     <div>
+      {loading && (
+        <div
+          style={{
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          loading
+        </div>
+      )}
       {options.map((o) => (
         <ReactECharts key={o.title.text} option={o as any} />
       ))}
